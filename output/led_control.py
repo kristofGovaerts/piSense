355c1853c1fb3324267db4da2c643eccdf55b5b8
kristofGovaerts <kristof88@gmail.com>
2021-07-17 20:13:15 +0200

    inital commits

--- output/led_control.py
+++ output/led_control.py
@@ -3,16 +3,14 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-LED = 16
-ledState = False
-GPIO.setup(LED, GPIO.OUT)
 
-try:
-    while True:
-        ledState = not ledState
-        GPIO.output(LED, ledState)
-        time.sleep(0.5)
-except KeyboardInterrupt:
-    print("Shutting down.")
-    ledState = False
-    GPIO.output(LED, ledState)
+def activate_led(id, duration):
+    GPIO.setup(id, GPIO.OUT)
+    GPIO.output(id, True)
+    time.sleep(duration)
+    GPIO.output(id, False)
+
+
+if __name__ == '__main__':
+    activate_led(16, 5)
+    activate_led(26, 10)

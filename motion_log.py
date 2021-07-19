c930f979eb53d3f7ce63ad7dd390c1bd31d86645
kristofGovaerts <kristof88@gmail.com>
2021-07-19 11:38:36 +0200

    Updated time monitoring.

--- motion_log.py
+++ motion_log.py
@@ -1,12 +1,12 @@
 """
 this code senses motion, and activates LEDs & provides environment stats when activated.
 """
-from output.led_control import activate_leds
+
 from sensors.sense import sense_temp_hum, sense_motion
 import adafruit_dht
 import RPi.GPIO as GPIO
 import time
-from tools.time import current_time
+from tools.time import current_time, is_active, timestr_to_delta
 
 PIR_PIN = 23  # motion detector
 DHT11_PIN = 17  # temp/hum
@@ -21,10 +21,29 @@ GPIO.setup(RED_PIN, GPIO.OUT)
 
 while True:
     # todo: respond to high-frequency motion by changing led color.
+    last20 = [None] * 20  # initialize
+    last_motion = current_time()
+
     if sense_motion(PIR_PIN):
-        activate_leds([GREEN_PIN], [2])
         h, t = sense_temp_hum(sensor, wait=0)
         ts = current_time()  # timestamp as str
+        last_motion = timestr_to_delta(ts)
+
         print("""Timestamp: {} --- Temperature: {}, humidity: {}""".format(ts, t, h))
+        d = last20.pop(0)  # remove oldest imestamp
+        last20.append(ts)
+
+        # red LED flickers for 1s for each motion. Green pin activates if is_active, otherwise it shuts down.
+        GPIO.output(RED_PIN, True)
+        if is_active(last20):
+            GPIO.output(GREEN_PIN, True)
+        else:
+            GPIO.output(GREEN_PIN, False)
+        time.sleep(1)
+        GPIO.output(RED_PIN, False)
+
+    delta = timestr_to_delta(current_time()) - last_motion
+    if delta.total_seconds() > 20:
+        GPIO.output(GREEN_PIN, False)
 
     time.sleep(1)
--- tools/time.py
+++ tools/time.py
@@ -6,4 +6,31 @@ def current_time():
 
 
 def timestr_to_delta(timestr):
-    return datetime.strptime(a, "%Y-%m-%d %H:%M:%S")
+    return datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
+
+
+def is_active(timestamps, f=30, t=20):
+    """
+    Determine whether or not there is active (high-frequency) motion in a list of timestamps.
+    :param timestamps: A list of timestamps, should be ordered chronologically.
+    :param f: An int. Frequency threshold. If there are more timestamps per minute than f, function returns True.
+    False otherwise.
+    :param t: An int. Determines the minimum amount of observations necessary. If len(timestamps) < t, function will
+    return False.
+    :return: True or False.
+    """
+    n = len(timestamps)
+
+    if n < t:
+        out = False
+    else:
+        delta = timestr_to_delta(timestamps[-1])- timestr_to_delta(timestamps[0])
+        freq = n/(delta.total_seconds()/60)  # convert to per min rather than per sec
+        if freq >= f:
+            print(freq)
+            out = True
+        else:
+            print(freq)
+            out = False
+
+    return out

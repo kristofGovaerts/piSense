9e91a4ef077ef63ff257fb01ae4724bac0cfd93a
kristofGovaerts <kristof88@gmail.com>
2021-07-24 10:26:55 +0200

    updated camera controls

--- motion_log.py
+++ motion_log.py
@@ -11,23 +11,24 @@ from tools.time import current_time, is_active, timestr_to_delta
 from sensors.camera import camera_start, camera_stop, take_photo
 
 # globals
-ACTIVITY_NUM = 2  # number of activations to cache, minimum amount to caclulate frequency from
+ACTIVITY_NUM = 3  # number of activations to cache, minimum amount to caclulate frequency from
 ACTIVITY_THRESH = 15  # frequency threshold in activations per minute.
 ACTIVITY_STOP = 3  # max inactive time allowed in seconds before the activity flag is turned off again.
+PHOTO_INTERVAL = 60  # one photo each minute
 
 # define pins
 PIR_PIN = 23  # motion detector
 DHT11_PIN = 17  # temp/hum
-GREEN_PIN = 26  # green led
-RED_PIN = 16  # red led
+# GREEN_PIN = 26  # green led
+# RED_PIN = 16  # red led
 
 # initialize sensors
 sensor = adafruit_dht.DHT11(DHT11_PIN)
 GPIO.setup(PIR_PIN, GPIO.IN)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
-GPIO.output(GREEN_PIN, False)  # turn off
-GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.output(RED_PIN, False)
+# GPIO.setup(GREEN_PIN, GPIO.OUT)
+# GPIO.output(GREEN_PIN, False)  # turn off
+# GPIO.setup(RED_PIN, GPIO.OUT)
+# GPIO.output(RED_PIN, False)
 
 # initialize reporting
 save_report()  # initialize file
@@ -35,10 +36,9 @@ lastN = [None] * ACTIVITY_NUM  # initialize
 last_motion = timestr_to_delta(current_time())
 active = False
 current_name = current_time()
+last_photo = timestr_to_delta(current_time())
 
 while True:
-    # TODO: try sensing less often
-    # TODO: always turn off LEDs after script
     if sense_motion(PIR_PIN):
         h, t = sense_temp_hum(sensor, wait=0)
         ts = current_time()  # timestamp as str
@@ -48,33 +48,44 @@ while True:
         lastN.append(ts)
 
         # red LED flickers for 1s for each motion. Green pin activates if is_active, otherwise it shuts down.
-        GPIO.output(RED_PIN, True)
+        # GPIO.output(RED_PIN, True)
         output = """Timestamp: {} --- Temperature: {}, humidity: {}, is_active: {}"""
         if is_active(lastN, t=ACTIVITY_NUM, f=ACTIVITY_THRESH):
             # here we want to start filming until is_active becomes false.
             if not active:
-                # this means it's the start of the active period
+                # this means it's the start of the active period. Take a photo and send.
                 active = True
                 current_name = current_time()
                 take_photo(current_name + '.jpg')
                 send_alert(current_name + '.jpg', output.format(current_name, t, h, active))
-                #camera_start(name=current_name + '.h264')
-            GPIO.output(GREEN_PIN, True)
+                last_photo = timestr_to_delta(current_name)
+            else:
+                # just take a photo, don't send an alert.
+                current_name = current_time()
+                take_photo(current_name + '.jpg')
+                last_photo = timestr_to_delta(current_name)
+            # GPIO.output(GREEN_PIN, True)
             active = True
         else:
-            # here we want to take a picture and send
             GPIO.output(GREEN_PIN, False)
             # active = False
 
         print(output.format(ts, t, h, active))
         add_line([ts, t, h, active])
         time.sleep(0.5)
-        GPIO.output(RED_PIN, False)
+        # GPIO.output(RED_PIN, False)
 
-    delta = timestr_to_delta(current_time()) - last_motion
+    ct = timestr_to_delta(current_time())
+    delta = ct - last_motion
     if delta.total_seconds() > ACTIVITY_STOP:
-        GPIO.output(GREEN_PIN, False)
+        # GPIO.output(GREEN_PIN, False)
         active = False
         #camera_stop()
+
+    photodelta = ct - last_photo
+    if photodelta.total_seconds() > PHOTO_INTERVAL:
+        current_name = current_time()
+        take_photo(current_name + '_BG.jpg')
+        last_photo = ct
 
     time.sleep(1)
--- sensors/camera.py
+++ sensors/camera.py
@@ -1,32 +1,31 @@
 from picamera import PiCamera
+import picamera.array
 import time
 from tools.time import current_time
+import cv2
+import numpy as np
 
 camera = PiCamera()
 
 
-def camera_start(name=None):
-    if name is None:
-        name = current_time()  + '.h264'
-    camera.start_preview()
-    camera.start_recording(name)
+def get_frame(bw=False):
+    with picamera.array.PiRGBArray(camera) as output:
+        camera.resolution = (1280, 720)
+        camera.capture(output, 'rgb')
+        out = output.array
+        print('Captured %dx%d image' % (
+                output.array.shape[1], output.array.shape[0]))
+    if bw:
+        out = cv2.cvtColor(a, cv2.COLOR_RGB2GRAY)
+    return out
 
 
-def camera_stop():
-    camera.stop_recording()
-    camera.stop_preview()
-
-
-def take_photo(name=None):
+def take_photo(name=None, exposure=1.0):
     if name is None:
         name = current_time() + '.jpg'
     print("Taking a photo...")
     camera.start_preview()
-    time.sleep(5)
+    time.sleep(exposure)
     camera.capture(name)
     camera.stop_preview()
 
-if __name__ == '__main__':
-    print("Recording for five secs...")
-    camera_start(name='test.h264')
-    camera_stop()

aa803d6e95353c2a6c4dcd06e165a0bd2331a5a8
kristofGovaerts <kristof88@gmail.com>
2022-04-20 20:25:28 +0200

    updated plotting

--- climate_visualization.py
+++ climate_visualization.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
+import os
+from scipy.ndimage.filters import gaussian_filter1d
+
+LOCATION = r'C:\Users\Kristof\Desktop\testPi'
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('climate_log.csv', sep='\t')
-    df['temperature'] = pd.to_numeric(df['temperature'], errors='coerce')
-    df['humidity'] = pd.to_numeric(df['humidity'], errors='coerce')
+    df = pd.read_csv(os.path.join(LOCATION, 'climate_log.csv'), sep='\t')
+    df['temperature'] = gaussian_filter1d(pd.to_numeric(df['temperature'], errors='coerce'), sigma=2)
+    df['humidity'] = gaussian_filter1d(pd.to_numeric(df['humidity'], errors='coerce'), sigma=2)
     df['time'] = pd.to_datetime(df['time'])
 
-    plt.subplot(121)
-    plt.plot(df['time'], df['temperature'])
-    plt.subplot(122)
-    plt.plot(df['time'], df['humidity'])
+    fig, ax = plt.subplots(ncols=2)
+    g1 = sns.lineplot('time', 'temperature', data=df, ax=ax[0])
+    ax[0].tick_params(labelrotation=45)
+    g2 = sns.lineplot('time', 'humidity', data=df, ax=ax[1])
+    ax[1].tick_params(labelrotation=45)
